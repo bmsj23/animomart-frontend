18b4d71872f5ed4765b2f9159aee0c3fdf771da0
Benedict San Juan <benedict_sanjuan@dlsl.edu.ph>
2025-10-25 02:17:02 +0800

    refactor: make footer optional in MainLayout and keep toast always present

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -2,14 +2,14 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import Toast from '../common/Toast';
 
-const MainLayout = ({ children }) => {
+const MainLayout = ({ children, hideFooter = false }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-1 bg-gray-50">
         {children}
       </main>
-      <Footer />
+      {!hideFooter && <Footer />}
       <Toast />
     </div>
   );
