61265dfd1b20cff8544c3dbb07718a16fe2e0256
Ernalene Roocha <ernalene_rocha@dlsl.edu.ph>
2025-11-01 21:16:06 +0800

    style(cart): make the add to move on wishlist

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -406,7 +406,7 @@ const Cart = () => {
               onClick={() => moveSelectedToWishlist()}
               className="px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors cursor-pointer"
             >
-              Add to Wishlist
+              Move to Wishlist
             </button>
           </div>
         </div>
